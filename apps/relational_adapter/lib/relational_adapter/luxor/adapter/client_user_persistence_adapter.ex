ec1c39f34356486bcf50956c26dc3911a1d2e198
agnaldo4j <agnaldo4j@gmail.com>
2017-03-04 22:43:44 -0300

    working with changesets

--- apps/relational_adapter/lib/relational_adapter/luxor/adapter/client_user_persistence_adapter.ex
+++ apps/relational_adapter/lib/relational_adapter/luxor/adapter/client_user_persistence_adapter.ex
@@ -17,13 +17,20 @@ defmodule RelationalAdapter.Luxor.ClientUserPersistenceAdapter do
     end
 
     def handle_call({:save_client_user, client_user}, _from, actual_state) do
-        result = RelationalAdapter.Luxor.Repository.transaction fn ->
+        execute_transaction(client_user) |>
+        process_result_transaction(actual_state)
+    end
+
+    defp execute_transaction(client_user) do
+        RelationalAdapter.Luxor.Repository.transaction fn ->
             client_user |>
             RelationalAdapter.Luxor.ClientUser.from_business |>
             RelationalAdapter.Luxor.ClientUserRepository.save |>
             RelationalAdapter.Luxor.ClientUser.to_business
         end
+    end
 
+    defp process_result_transaction(result, actual_state) do
         case result do
             {:ok, saved_client_user} -> save_client_user_response(saved_client_user.client, actual_state)
             {:error, error} -> save_client_user_response(error, actual_state)
