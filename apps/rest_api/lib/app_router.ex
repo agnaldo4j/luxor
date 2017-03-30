ac9e856bc52b6ce143a825db55708d14e64604dc
agnaldo4j <agnaldo4j@gmail.com>
2017-03-30 12:40:53 -0300

    fix optional objects

--- apps/rest_api/lib/app_router.ex
+++ apps/rest_api/lib/app_router.ex
@@ -22,7 +22,15 @@ defmodule AppRouter do
         |> Command.User.AuthenticationUserCommand.new_from
         |> Usecase.Luxor.UserUsecaseApi.authenticate
 
-        send_resp(conn, 200, "deu certo")
+        case result do
+          {:error, errors} ->
+            {_, json} = Poison.encode(%{code: 401, message: "Invalid email or password"})
+            send_resp(conn, 401, json)
+          {:ok, user} ->
+            {_, json} = Poison.encode(user)
+            send_resp(conn, 200, json)
+        end
+
     end
 
     forward "/v1/users", to: UserRouter
